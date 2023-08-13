eb63cf4bd92d57c72bf4ca243239b8c7fa00d28f
Jamiu Olanrewaju <96052702+techwithlanre@users.noreply.github.com>
2023-08-13 12:27:03 +0100

    sunday push

---
+++ app/Http/Controllers/WorkbenchController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use DateTime;
+use Illuminate\Http\Request;
+
+class WorkbenchController extends Controller
+{
+    public function index()
+    {
+        $date = new DateTime();
+        $date->modify('+1 weekday');
+        $shipment_date = $date->format('Y-m-d 10:00:00');
+        echo $shipment_date = str_replace(' ', 'T', $shipment_date) . " GMT+01:00";
+    }
+
+    public function render()
+    {
+        $offset = 0;
+
+        if (isset($_POST['user_input_box']) && isset($_POST['search']) && isset($_POST['replace'])) {
+
+            $user_input_box = $_POST['user_input_box'];
+            $search = $_POST['search'];
+            $replace = $_POST['replace'];
+
+            //To check whether above code work or not
+
+            //  echo $user_input_box = $_POST['user_input_box'];
+            //  echo $search = $_POST['search'];
+            //  echo $replace = $_POST['replace'];
+
+            $search_length = strlen($search);
+
+            if (!empty($_POST['user_input_box']) && !empty($_POST['search']) && !empty($_POST['replace'])) {
+
+                while ($string_position = strpos($user_input_box, $search, $offset)) {
+                    // echo $string_position."<br>";
+                    // echo $offset = $string_position + $search_length;
+
+                    $offset = $string_position + $search_length;
+                    $user_input_box = substr_replace($user_input_box, $replace, $string_position, $search_length);
+                }
+            }
+            echo $user_input_box;
+        }
+
+        else{
+            echo "Please fill the complete fields.";
+        }
+
+        echo '<form method="POST" action="#">
+    
+    <textarea cols="34" rows="12" name="user_input_box"></textarea>
+    <br><br>
+    <label>Search For: </label> <br>
+    <input type="text" name="search"><br><br>
+    <label>Replace With: </label><br>
+    <input type="text" name="replace"><br><br>
+    <input type="submit" value="Find and Replace">
+ </form>';
+    }
+}
